bd3a39e4b842b39039613e70bdc2eb3a02abc112
CN72 <cn72@CN72.local>
2019-09-18 10:40:55 +0100

    made separate component files

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <footer>Made with React</footer>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <button>Home</button>
+            <button>Portfolio</button>
+            <button>More about me</button>
+
+        </div>
+    )
+}
+
+export default Navbar
