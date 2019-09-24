6af6cba7df68a11fe265f3346186b9436a580c66
CN72 <cn72@CN72.local>
2019-09-24 16:10:52 +0100

    Updates

---
+++ precache-manifest.2a92b099ed8b369f2fba70448aed5312.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f6c1547f6b5080724ef8c8141f9fb4d",
+    "url": "/portfolio-react/./index.html"
+  },
+  {
+    "revision": "b0c9420d5c872ea556ea",
+    "url": "/portfolio-react/./static/css/main.dc6430bf.chunk.css"
+  },
+  {
+    "revision": "1a9a579a9b5c4395ba08",
+    "url": "/portfolio-react/./static/js/2.98162777.chunk.js"
+  },
+  {
+    "revision": "b0c9420d5c872ea556ea",
+    "url": "/portfolio-react/./static/js/main.1b7f3b05.chunk.js"
+  },
+  {
+    "revision": "459f8c042e1b80954a51",
+    "url": "/portfolio-react/./static/js/runtime~main.1cd29dda.js"
+  },
+  {
+    "revision": "96d69d4ec1ceee042a79b60980e5d737",
+    "url": "/portfolio-react/./static/media/black-dice-game.96d69d4e.png"
+  },
+  {
+    "revision": "4458bf26a3c66683408626b791a2c99d",
+    "url": "/portfolio-react/./static/media/cyber-pet-game.4458bf26.png"
+  },
+  {
+    "revision": "0e2c253b6d7662b08b972f3a9b432783",
+    "url": "/portfolio-react/./static/media/drumkit-cat.0e2c253b.png"
+  },
+  {
+    "revision": "f0ebc33ab0aa128225f3a31f10ce73e0",
+    "url": "/portfolio-react/./static/media/keycode-generator.f0ebc33a.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio-react/./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b42824debd128038325a23fe907f3b5b",
+    "url": "/portfolio-react/./static/media/london-bus-animation.b42824de.png"
+  },
+  {
+    "revision": "80fb2b181eab4688268ef62f77aecd25",
+    "url": "/portfolio-react/./static/media/netflix-copy.80fb2b18.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-react/./precache-manifest.f63c7d41e8c90983c16a9703ab3b0e74.js"
+  "/portfolio-react/./precache-manifest.2a92b099ed8b369f2fba70448aed5312.js"
 );
 
 self.addEventListener('message', (event) => {
